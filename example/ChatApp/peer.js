d9fc770beb165298befc924fa60d38e759b2cef4
Devin Haslam <dhasl002odu.edu>
2018-01-04 18:37:34 -0500

    sending files finished

--- example/ChatApp/peer.js
+++ example/ChatApp/peer.js
@@ -1,6 +1,12 @@
 var smoke = require('../../index')
+var fs = require('fs');
+const readline = require('readline');
+var prompt = require('prompt');
 
 process.stdin.setEncoding('utf8')
+
+var readableStream = fs.createReadStream('test.txt');
+var myFile = fs.createWriteStream('incoming.txt');
 
 var node = smoke.createNode({
   port: parseInt(process.argv[2]) || 5000
@@ -16,16 +22,22 @@ console.log('Connecting...');
 
 node.on('connect', function() {
   console.log('Connected. Happy chatting!\n');
+  
 })
 
 node.on('disconnect', function() {
   console.log('Disconnected. Sorry.');
 })
 
-var fs = require('fs');
-var readableStream = fs.createReadStream('test.txt');
+var str = "";
+prompt.start();
+prompt.get(['input'], function (err, result) {
+	if(result.input == "send")
+	{
+		console.log('  input: ' + result.input);
+		readableStream.pipe(node.broadcast).pipe(myFile)
+	}
+});
 
-// Send message
-readableStream.pipe(node.broadcast).pipe(process.stdout)
 node.on('error', function(e) {throw e})
 node.start()
